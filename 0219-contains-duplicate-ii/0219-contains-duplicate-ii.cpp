0ae69140eb8ed4e854291bbc3f62440faf011d95
ALOK SINGH <aloks90366@gmail.com>
2026-08-02 18:25:15 +0530

    Time: 103 ms (27.2%), Space: 124.9 MB (10.58%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> f; 
+        for (int i = 0; i < nums.size(); i++) {
+            if (f.count(nums[i]) && i - f[nums[i]] <= k) {
+                return true;
+            }
+            f[nums[i]] = i; 
+        }
+        return false;
+    }
+};

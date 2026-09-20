6d32b4aa2c6fef30a069247c5763016851764d28
ALOK SINGH <aloks90366@gmail.com>
2026-09-20 10:58:23 +0530

    Time: 0 ms (100%), Space: 63.6 MB (48.51%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -4,11 +4,13 @@ public:
         int l = 0;
         int h = arr.size() - 1;
         while (l < h) {
-            int mid = l + (h - l) / 2;
-            if (arr[mid] < arr[mid + 1])
-                l = mid + 1;
-            else
-                h = mid;
+            int g = l + (h - l) / 2;
+            if (arr[g] < arr[g + 1]) {
+                l = g + 1;
+            }
+            else {
+                h = g;
+            }
         }
         return l;
     }
